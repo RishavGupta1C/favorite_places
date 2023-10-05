19d1af09aaa19bbf30685565ae10b36de2a6c28b
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-10-05 23:52:10 +0530

    forth commit

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,6 +1,10 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 class Place {
   final String id;
   final String title;
 
-  const Place({required this.title});
+  Place({required this.title}) : id = uuid.v4();
 }
